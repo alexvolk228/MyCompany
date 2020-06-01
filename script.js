bcdd640343df262c0cd51b397dd239b969531ace
alexvolk228 <aleksey_volk@mail.ru>
2020-06-01 18:44:28 +0300

    Finished Site

---
+++ script.js
@@ -0,0 +1,9 @@
+menu.onclick = function myFunction() {
+    var x = document.getElementById('myTopnav');
+    if (x.className === "topnav") {
+        x.classList.add("responsive");
+    }
+    else {
+        x.className = "topnav";
+    }
+ }
